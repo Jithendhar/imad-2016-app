280c68e0989b9edd9c4b67638786f5a65442a116
Jithendhar <Jithendharjith@gmail.com>
2016-09-23 17:09:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,12 @@ var element = document.getElementById('main-text');
 element.innerHTML ='newv value';
 
 var image = document.getElementById('madi');
+marginLeft = 0;
+function moveRight () 
+{
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px'
+}
 img.onclick = function(){
   var interval=setInterval(moveRight,100);
 }
